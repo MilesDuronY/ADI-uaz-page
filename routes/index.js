bb2871161f550d97dd3c03b7af8600e2f6e9b7ae
MilesDuron <milesduron@uaz.edu.mx>
2020-06-18 13:50:00 -0500

    Aplicación de templates con EJS

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { page: 'home' });
+});
+router.get('/contacto', function(req, res, next) {
+  res.render('contacto', { page: 'contacto', ligas:['https://www.facebook.com', 'https://www.twitter.com','https://www.instagram.com','https://www.blabla.com'] });
 });
 
 module.exports = router;
